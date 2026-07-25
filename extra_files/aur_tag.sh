5064afc5df90bb6ad2ad328caff38a77f13fdb63
Xmlzitos154 <matheuz.ricardo@protonmail.com>
2026-07-25 19:47:24 -0300

    chore: add aur_tag and updater.sh

---
+++ extra_files/aur_tag.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+## This file exists for non-aur cly users, so they can update the program using 'cly updater'.
+## This file also can be used to verify if the latest version is uploaded on aur.
+
+# upload date:    2026-07-25
+# init version:   v7.5.4-2
+# actual version: v7.5.5-1
