83e66fb243173d62f3f3f483b233c3ef9c6d1ea8
Xmlzitos154 <matheuz.ricardo@protonmail.com>
2026-08-06 21:02:28 -0300

    add aur note and bump version to v7.5.7-2

--- extra_files/aur_tag.sh
+++ extra_files/aur_tag.sh
@@ -3,6 +3,6 @@
 ## This file exists for non-aur cly users, so they can update the program using 'cly updater'.
 ## This file also can be used to verify if the latest version is uploaded on aur.
 
-# upload date:    2026-08-2
+# upload date:    2026-08-06
 # init version:   v7.5.4-2
-# actual version: v7.5.7-1
+# actual version: v7.5.7-2
